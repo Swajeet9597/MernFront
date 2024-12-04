053cad62865477130450ec501fb7c8eb366b6937
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-05 00:34:06 +0530

    up

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './NavBar2.css'
 import logo from '../assets/LOGO.png'
 import user from '../assets/User.png'
+import { useNavigate } from 'react-router-dom';
 const NavBar2 = () => {
+
+
+        const [userData, setuserData] = useState([])
+
+        const navigate = useNavigate()
+
+        
+const handleBack=()=>{
+        navigate('/Profile')
+    }
+
+    
+        const emailInfo = {
+            "email": localStorage.getItem("email")
+        }
+    
+        async function getUserData () {
+          const response = await fetch(`${BASE_URL}/api/auth/user`,{
+            method:"POST",
+            headers:{
+              "Content-Type":"application/json"
+            },
+            body: JSON.stringify(emailInfo)
+          })
+    
+          if(response.ok){
+            let data = await response.json()
+    
+            setuserData(data.msg)
+          }
+        }
+        useEffect(()=>{
+                getUserData()
+              },[])    
+        
   return (
     <div className='Navbar2' >
         <div className="navbox1">
@@ -15,10 +51,10 @@ const NavBar2 = () => {
                 <span>Track Order</span>
         </div>
         <div className='profile' > 
-        <div className='profile1' >
+        <div onClick={handleBack} className='profile1' >
 
                 <img src={user} alt="" className="user" />
-                <span>hey Mike</span>
+                <span>hey {userData.name}</span>
         </div>
         </div>
     </div>
