698f8858930ce20497b1facf34d4254a74c90632
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-04 18:07:44 +0530

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,6 +11,14 @@ import NavbarM1 from '../mobileComponents/NavbarM1';
 
 const Profile = () => {
 
+    const [activeEditProfile, setActiveEditProfile] = useState(true);
+
+    const handleEditBtn = () => {
+      setActiveEditProfile(false);
+      // console.log("active");
+    };
+  
+
     const getScreen = () =>{
         return{
           width:window.innerWidth,
@@ -40,6 +48,87 @@ const Profile = () => {
  <NavBarOrder/>
 </div>
 
+
+        {activeEditProfile ? (
+          <div className="profileBody">
+            <p className="bodyHead">
+              <img onClick={handleBack} src="arrow-left.png" alt="" />
+              <img src="backIcon.png" alt="" />
+              My Profile
+            </p>
+            <div className="body_profile">
+              <div className="profileHead">
+                <div>
+                  <img src="profilePic.png" alt="" />
+                  <p>Mike Ross</p>
+                </div>
+                <div onClick={handleEditBtn} className="editBtn">
+                  Edit
+                </div>
+              </div>
+              <div className="profileData">
+                <div className="name_gender">
+                  <div className="fname">
+                    <p>Full Name</p>
+                    <p>mike ross</p>
+                  </div>
+                  <div className="gender">
+                    <p>Gender</p>
+                    <p>Male</p>
+                  </div>
+                </div>
+                <div className="email_country">
+                  <div className="emailAdd">
+                    <p>Email Address</p>
+                    <p>mikeross@gmail.com</p>
+                  </div>
+                  <div className="country">
+                    <p>Country</p>
+                    <p>India</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="paymentMethod">
+              <p>Saved Payment Methods</p>
+              <div className="PaymentCards">
+                <div className="paymentCard">
+                  <img src="cardPic.png" alt="" />
+                  <div className="cardDetails">
+                    <p>xxxx xxxx xxxx 1234</p>
+                    <p>Mike Ross</p>
+                  </div>
+                  <img src="edit-3.png" alt="" />
+                </div>
+                <div className="paymentCard">
+                  <img src="cardPic.png" alt="" />
+                  <div className="cardDetails">
+                    <p>xxxx xxxx xxxx 1234</p>
+                    <p>Mike Ross</p>
+                  </div>
+                  <img src="edit-3.png" alt="" />
+                </div>
+                <div className="paymentCard">
+                  <img src="cardPic.png" alt="" />
+                  <div className="cardDetails">
+                    <p>xxxx xxxx xxxx 1234</p>
+                    <p>Mike Ross</p>
+                  </div>
+                  <img src="edit-3.png" alt="" />
+                </div>
+                <div className="addCardBtn">
+                  <img src="btnImg3.png" alt="" />
+
+                  <p>Add New Card</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="profileEdit">
+            <ProfileEditPg setActiveEditProfile={setActiveEditProfile} />
+          </div>
+        )}
 
 
 
