423591cee854f38fbb26ce980bec5a7550d28961
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-05 00:47:27 +0530

    up

--- src/components/NavBar2P.jsx
+++ src/components/NavBar2P.jsx
@@ -2,7 +2,42 @@ import React from 'react';
 import './NavBar2P.css'
 import logo from '../assets/LOGO.png'
 import user from '../assets/User.png'
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../services/helper';
 const NavBar2P = () => {
+        const [userData, setuserData] = useState([])
+
+        const navigate = useNavigate()
+
+        
+const handleBack=()=>{
+        navigate('/Profile')
+    }
+
+    
+        const emailInfo = {
+            "email": localStorage.getItem("email")
+        }
+    
+        async function getUserData () {
+          const response = await fetch(`${BASE_URL}/api/auth/user`,{
+            method:"POST",
+            headers:{
+              "Content-Type":"application/json"
+            },
+            body: JSON.stringify(emailInfo)
+          })
+    
+          if(response.ok){
+            let data = await response.json()
+    
+            setuserData(data.msg)
+          }
+        }
+        useEffect(()=>{
+                getUserData()
+              },[])    
+        
   return (
     <div className='Navbar2' >
     <div className="navbox1">
@@ -18,7 +53,7 @@ const NavBar2P = () => {
     <div className='profile1' >
 
             <img src={user} alt="" className="user" />
-            <span>hey Mike</span>
+            <span>hey {userData.name}</span>
     </div>
     </div>
 </div>
