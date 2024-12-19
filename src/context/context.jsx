669895a23ccd58261cce37c4b0136e8cead31f34
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-19 15:20:56 +0530

    uppp

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const Usercontext = createContext()
 
-export const Userprovider = async({Children})=>{
+export const Userprovider = ({Children})=>{
  
     const [currentUser, setCurrentUser] = useState(null)
 
