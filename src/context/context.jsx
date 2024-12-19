b777dbf6e6b78a4c38635d7cbd2fa4d7d795f348
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-19 15:23:48 +0530

    uppp

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -2,12 +2,12 @@ import { createContext, useState } from "react";
 
 export const Usercontext = createContext()
 
-export const Userprovider = ({Children})=>{
+export const Userprovider = ({children})=>{
  
     const [currentUser, setCurrentUser] = useState(null)
 
     return <Usercontext.Provider value={{currentUser,setCurrentUser}} >
-             {Children}
+             {children}
             </Usercontext.Provider>
 }
 
